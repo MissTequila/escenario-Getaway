1317141554f7ca6dd010b16f14f19f05a4463b5b
malebel98 <malebel98@gmail.com>
2017-10-05 23:08:12 -0500

    Player
    Script del personaje

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody bullet;
+    public float speed;
+    public GameObject player;
+
+    void Fire()
+    {
+        Rigidbody bulletClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletClone.velocity = transform.forward * speed;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Shoot"))
+        {
+            Fire();
+        }
+    }
+}
