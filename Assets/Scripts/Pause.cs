2f3fb200363a866edd9e28a026254b34e1bdbd19
malebel98 <malebel98@gmail.com>
2017-10-20 04:11:39 -0500

    menus, FPS, puntero
    menus, FPS, puntero

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseBtn, pauseMenu;
+
+    public void OnPause()
+    {
+        pauseMenu.SetActive(true);
+        pauseBtn.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    public void OffPause()
+    {
+        pauseMenu.SetActive(false);
+        pauseBtn.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu(string backtomenu)
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(backtomenu);
+    }
+
+    public void ExitGamebtn()
+    {
+        Time.timeScale = 1;
+        Application.Quit();
+    }
+}
